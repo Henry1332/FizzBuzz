18307f5331d72e70e03343f5e4de901d1488e38f
Henry1332 <1774004479@qq.com>
2025-09-11 00:25:52 -0400

    Make doFizzBuzz public and test in Main

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -10,18 +10,18 @@ class FizzBuzz {
 
         while (i < 100) {
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
-
-            doFizzBuzz(divisibleBy3, divisibleBy5, i);
+            doFizzBuzz(i);
             i++;
 
         }
 
     }
 
-    private static void doFizzBuzz(boolean divisibleBy3, boolean divisibleBy5, int i) {
+    public static void doFizzBuzz(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
         // Print our appropriate result.
         if (divisibleBy3 && divisibleBy5) {
 
