f065d33350fa8c51168b19a33a86a617901caa11
Henry1332 <1774004479@qq.com>
2025-09-11 22:33:35 -0400

    Finish task4

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -1,4 +1,22 @@
 package lab1;
 
-public class Multiple {
+public class Multiples {
+    public static void main(String[] args){
+        int count = multiples();
+        System.out.println(count);
+    }
+    public static int multiples(){
+        return multiples(1000,3,5);
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                count++;
+            }
+
+        }
+        return count;
+    }
 }
