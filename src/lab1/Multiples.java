6b08f0de660f93134e2d67573d73f805fe9c66fe
Henry1332 <1774004479@qq.com>
2025-09-25 09:22:21 -0400

    just a try

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -14,6 +14,7 @@ public class Multiples {
         for (int i = 1; i < n; i++) {
             if (i % a == 0 || i % b == 0) {
                 count++;
+
             }
         }
         return count;
